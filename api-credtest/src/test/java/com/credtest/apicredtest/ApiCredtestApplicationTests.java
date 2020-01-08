7f9135d02f4aa9d5afd43f5f19b968067867162f
Alan Lopes <arlopes.nit@hotamail.com>
2020-01-08 13:53:05 -0300

    Fontes iniciais

---
+++ api-credtest/src/test/java/com/credtest/apicredtest/ApiCredtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credtest.apicredtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCredtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
